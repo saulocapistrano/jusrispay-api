73988fd81301c469cb00eadef44aac6a1c1bde28
saulocapistrano <saulocapistrano@gmail.com>
2025-12-12 14:01:22 -0300

    feat: implement PaymentApplicationMapper for converting between Payment DTOs and domain models

---
+++ src/main/java/br/com/jurispay/application/payment/mapper/PaymentApplicationMapper.java
@@ -0,0 +1,26 @@
+package br.com.jurispay.application.payment.mapper;
+
+import br.com.jurispay.application.payment.dto.PaymentRegistrationCommand;
+import br.com.jurispay.application.payment.dto.PaymentResponse;
+import br.com.jurispay.domain.payment.model.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * Mapper para conversão entre DTOs de aplicação e modelos de domínio de Payment.
+ */
+@Mapper(componentModel = "spring")
+public interface PaymentApplicationMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "diasAtraso", ignore = true)
+    @Mapping(target = "multaTotal", ignore = true)
+    @Mapping(target = "valorFinalRecebido", ignore = true)
+    @Mapping(target = "roiBrl", ignore = true)
+    @Mapping(target = "roiPercent", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    Payment toDomain(PaymentRegistrationCommand command);
+
+    PaymentResponse toResponse(Payment payment);
+}
+
