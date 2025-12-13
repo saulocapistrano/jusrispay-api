89b06fbed54c11674ced63e5516f223358dcdbcb
saulocapistrano <saulocapistrano@gmail.com>
2025-12-12 21:50:05 -0300

    feat: add CollectionChannel enum to define available collection channels

---
+++ src/main/java/br/com/jurispay/domain/collection/model/CollectionChannel.java
@@ -0,0 +1,12 @@
+package br.com.jurispay.domain.collection.model;
+
+/**
+ * Canais de cobrança disponíveis.
+ */
+public enum CollectionChannel {
+    WHATSAPP,
+    PHONE,
+    EMAIL,
+    IN_PERSON
+}
+
