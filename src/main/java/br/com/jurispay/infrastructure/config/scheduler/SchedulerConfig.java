7c7975fa5481a669a897f82cafd1dd65d25f78a5
saulocapistrano <saulocapistrano@gmail.com>
2025-12-12 21:55:53 -0300

    feat: add SchedulerConfig to enable scheduling in the application

---
+++ src/main/java/br/com/jurispay/infrastructure/config/scheduler/SchedulerConfig.java
@@ -0,0 +1,13 @@
+package br.com.jurispay.infrastructure.config.scheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Configuração para habilitar scheduling na aplicação.
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
+
