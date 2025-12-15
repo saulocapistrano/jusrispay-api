4b5f8260a97b23dd6bd4a3fb2ce278ee6a5ca8ce
saulocapistrano <saulocapistrano@gmail.com>
2025-12-14 22:19:07 -0300

    feat: add CommandValidator interface for command validation logic extraction

---
+++ src/main/java/br/com/jurispay/application/common/validator/CommandValidator.java
@@ -0,0 +1,21 @@
+package br.com.jurispay.application.common.validator;
+
+import br.com.jurispay.domain.common.exception.ValidationException;
+
+/**
+ * Interface genérica para validação de comandos.
+ * Permite extrair lógica de validação dos use cases para classes dedicadas.
+ *
+ * @param <T> tipo do comando a ser validado
+ */
+public interface CommandValidator<T> {
+
+    /**
+     * Valida o comando e lança ValidationException se houver erros.
+     *
+     * @param command comando a ser validado
+     * @throws ValidationException se o comando for inválido
+     */
+    void validate(T command);
+}
+
