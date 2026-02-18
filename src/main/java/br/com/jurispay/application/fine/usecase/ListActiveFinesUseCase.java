5799760e5cc166c289d6f685126f3cf906cb1ea4
saulocapistrano <saulocapistrano@gmail.com>
2026-02-18 05:48:10 -0300

    feat: adicionar caso de uso para listagem de multas ativas

---
+++ src/main/java/br/com/jurispay/application/fine/usecase/ListActiveFinesUseCase.java
@@ -0,0 +1,9 @@
+package br.com.jurispay.application.fine.usecase;
+
+import br.com.jurispay.api.dto.fine.FineResponse;
+
+import java.util.List;
+
+public interface ListActiveFinesUseCase {
+    List<FineResponse> listActive();
+}
