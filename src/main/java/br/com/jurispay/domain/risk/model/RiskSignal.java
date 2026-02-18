8c3640e126629f4bff9ed9ba26b398c910f972dc
saulocapistrano <saulocapistrano@gmail.com>
2026-02-18 06:53:13 -0300

    feat: adicionar enum RiskSignal com sinais de risco THIN_FILE, NO_HIT, WEIGHT_REBALANCED, CONFIDENCE_DISCOUNT_APPLIED e BUREAU_UNAVAILABLE

---
+++ src/main/java/br/com/jurispay/domain/risk/model/RiskSignal.java
@@ -0,0 +1,9 @@
+package br.com.jurispay.domain.risk.model;
+
+public enum RiskSignal {
+    THIN_FILE,
+    NO_HIT,
+    WEIGHT_REBALANCED,
+    CONFIDENCE_DISCOUNT_APPLIED,
+    BUREAU_UNAVAILABLE
+}
